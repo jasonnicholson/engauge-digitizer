d9b1c43f187b375558c1bd6505acfbe840c180b1
Jason H. Nicholson <1058191+jasonnicholson@users.noreply.github.com>
2026-04-09 21:56:10 -0400

    fix: update About dialog - new repo URL, remove Gitter, update copyright
    - Project Home Page now points to jasonnicholson/engauge-digitizer
    - Removed Gitter Forum link (defunct)
    - Copyright: © 2014 Mark Mitchell, © 2025 Jason Nicholson

--- src/Dlg/DlgAbout.cpp
+++ src/Dlg/DlgAbout.cpp
@@ -16,18 +16,17 @@ DlgAbout::DlgAbout (MainWindow &mainWindow) :
   setTextFormat (Qt::RichText);
 
   // Do not embed single quotes in the strings below since that will interfere with the translations
-  setText (QString ("<p>%1 %2 %3</p> <p>&copy; Mark Mitchell</p><p>%4</p><p>%5</p><p>%6</p><p>%7</p><p>%8:</p>"
+  setText (QString ("<p>%1 %2 %3</p> <p>&copy; 2014 Mark Mitchell, &copy; 2025 Jason Nicholson</p><p>%4</p><p>%5</p><p>%6</p><p>%7</p><p>%8:</p>"
                     "<ul>"
-                     "<li><a href=\"https://github.com/markummitchell/engauge-digitizer\">%9</a></li>"
-                     "<li><a href=\"https://gitter.im/markummitchell/engauge-digitizer\">%10</a></li>"
+                     "<li><a href=\"https://github.com/jasonnicholson/engauge-digitizer\">%9</a></li>"
                      "</ul>"
                      "<p>FFTW 3.X.X:</p>"
                      "<ul>"
-                     "<li><a href=\"http://www.fftw.org\">%11</a></li>"
+                     "<li><a href=\"http://www.fftw.org\">%10</a></li>"
                      "</ul>"
                      "<p>OpenJPEG 2.X:</p>"
                      "<ul>"
-                     "<li><a href=\"http://www.openjpeg.org\">%12</a></li>"
+                     "<li><a href=\"http://www.openjpeg.org\">%11</a></li>"
                     "</ul>")
            .arg (tr ("Engauge Digitizer"))
            .arg (tr ("Version"))
@@ -42,7 +41,6 @@ DlgAbout::DlgAbout (MainWindow &mainWindow) :
            .arg (tr ("Read the included LICENSE file for details."))
            .arg (tr ("Engauge Digitizer"))
            .arg (tr ("Project Home Page"))
-           .arg (tr ("Gitter Forum"))
            .arg (tr ("Project Page"))
            .arg (tr ("Project Page")));
 }
